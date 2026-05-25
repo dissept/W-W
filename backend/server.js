c830411b619dfa5796bb95860e8605a484ff7d47
dissept <162051466+dissept@users.noreply.github.com>
2026-05-25 11:25:25 +0200

    Add callback

--- backend/server.js
+++ backend/server.js
@@ -80,7 +80,6 @@ app.post("/api/denuncia", async (req, res) => {
   }
 });
 
-// START SERVER
-app.listen(3000)
+app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
